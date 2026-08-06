4f676ec7c3d8ba2389b931b9085ee82fce331a51
Arpita Halder <arpitahalder2311@gmail.com>
2026-08-06 17:23:42 +0600

    fix spacing

--- Drawing A Shape/main.c
+++ Drawing A Shape/main.c
@@ -4,10 +4,10 @@
 int main()
 {
     printf("\n");
-    printf("       /|\n");
-    printf("      / |\n");
-    printf("     /  |\n");
-    printf("    /   |\n");
-    printf("   /____|\n");
+    printf("       /| \n");
+    printf("      / | \n");
+    printf("     /  | \n");
+    printf("    /   | \n");
+    printf("   /____| \n");
     return 0;
 }
