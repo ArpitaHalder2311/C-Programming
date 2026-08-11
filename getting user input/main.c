bbe535cfa299bd84bf032f1bcb6a2d9e89563570
Arpita Halder <arpitahalder2311@gmail.com>
2026-08-11 23:47:41 +0600

    Add my practice works of C Programming

---
+++ getting user input/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double gpa;
+
+    printf("Enter your gpa: ");
+    scanf("%lf", &gpa);
+    printf("Your gpa is %lf ", gpa);
+    return 0;
+}
