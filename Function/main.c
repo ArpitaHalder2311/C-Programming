7a74f7e2d146ae3b1669ecccf332d42fa536ec90
Arpita Halder <arpitahalder2311@gmail.com>
2026-08-13 23:29:44 +0600

    Add my practice works of C Programming

---
+++ Function/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+void sayHi(){
+   printf("Hello User");}
+
+int main()
+{
+    printf("Top");
+    sayHi();
+    printf("bottom");
+    return 0;
+}
+
+
+
+
+
+
+
+
