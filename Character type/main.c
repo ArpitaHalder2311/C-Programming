4af347d37609f416776f01d6bb21f3bff83debaf
Arpita Halder <arpitahalder2311@gmail.com>
2026-08-17 21:57:54 +0600

    Add my practice works of C Programming

---
+++ Character type/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   char raju;
+    printf("Enter the first letter of your name: ");
+    scanf("%c", &raju);
+    printf("Enter the first letter of your name: %c\n" , raju);
+    return 0;
+}
