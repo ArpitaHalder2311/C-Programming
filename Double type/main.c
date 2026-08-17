0e8cd468978cf0b3f428fcaf8951cd97f901db5a
Arpita Halder <arpitahalder2311@gmail.com>
2026-08-17 21:25:40 +0600

    Add my practice works of C Programming

---
+++ Double type/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    double x;
+    x = 10.5;
+    n = x;
+    printf("Value of n is %d\n", n);
+    printf("Value of x is %lf\n",x);
+    return 0;
+}
