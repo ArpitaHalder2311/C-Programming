29c72fa84f42f40131ce5ddc064e3b2ae2e75257
Arpita Halder <arpitahalder2311@gmail.com>
2026-08-18 22:34:30 +0600

    Add my practice works of C Programming

---
+++ conditional logic/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n=10;
+
+    if (n >= 0){
+
+    printf("The number is positive\n");
+
+    }else{
+
+    printf("The number is negative\n");
+    }
+    return 0;
+}
