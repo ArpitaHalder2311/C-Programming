69e0fe2551f441eee578437e0043d54e569dbf7d
Arpita Halder <arpitahalder2311@gmail.com>
2026-08-18 23:37:16 +0600

    Add my practice works of C Programming

---
+++ even or odd number/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n=5, remainder;
+    remainder = n % 2;
+
+    if( remainder==0)
+
+
+    {printf("The number is even\n");
+
+    }else{
+
+
+
+     printf("The number is odd\n");
+    }
+
+    return 0;
+}
