cc221fd983aa8d3989157d8639490c6a75d1f79b
Arpita Halder <arpitahalder2311@gmail.com>
2026-08-22 00:39:50 +0600

    Add my practice works of C programming

---
+++ A number is divisible by 2 or not/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x;
+    printf("enter a number: ");
+    scanf("%d", &x);
+
+
+
+
+    printf("%d\n", x % 2 == 0);
+    return 0;
+}
