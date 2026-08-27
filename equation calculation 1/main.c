85de8c682d89b8dfe68a8697648a61e7aa42e21f
Arpita Halder <arpitahalder2311@gmail.com>
2026-08-27 21:42:00 +0600

    Add my practice works of C Programming

--- equation calculation 1/main.c
+++ equation calculation 1/main.c
@@ -3,21 +3,7 @@
 
 int main()
 {
-    int a1,a2,b1,b2,c1,c2,x,y;
-      printf(" a1 = ");
-      scanf("%lf",&a1);
-      printf(" a2 = ");
-      scanf("%lf",&a2);
-      printf(" b1 = ");
-      scanf("%lf",&b1);
-      printf(" b2 = ");
-      scanf("%lf",&c2);
-      printf(" c1 = ");
-      scanf("%lf",&c1);
-      printf(" c2 = ");
-      scanf("%lf",&c2);
-    x=(b2*c1 - b1*c2)/(a1*b2 - a2*b1);
-    y=(a1*c2 - a2*c1)/(a1*b2 - a2*b1);
-    printf("x=%0.21lf, y=%0.21lf\n", x, y);
+  int a=50,b=60;
+  printf("sum is %d", a+b);
     return 0;
 }
