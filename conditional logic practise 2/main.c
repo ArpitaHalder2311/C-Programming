99f697db0346a792199f82e84d2036c2e0e3b174
Arpita Halder <arpitahalder2311@gmail.com>
2026-08-31 21:54:02 +0600

    Add my practise works of C Programming

---
+++ conditional logic practise 2/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int g;
+    if(g >= 90){
+    printf("The grade is A\n");
+    }else if ( g >= 80){
+        printf("The grade is B\n");
+        }else if ( g >= 70){
+           printf("The grade is C\n");
+            }else if( g >= 60 ){
+             printf("The grade is D\n");
+              }else if( g < 60 ){
+                  printf("The grade is F\n");
+              }
+
+
+
+
+
+    return 0;
+}
