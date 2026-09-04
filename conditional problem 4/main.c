bf6844702b0cad60738aae6701cc3a2b26f6d698
Arpita Halder <arpitahalder2311@gmail.com>
2026-09-04 23:11:57 +0600

    Add my practice works of C Programming

---
+++ conditional problem 4/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int age;
+    printf("enter age :");
+    scanf("%d", &age);
+
+    if(age >=18){
+        printf("adult\n");
+    }else if (age>13 && age<18){
+        printf("teenager\n");
+    }else{
+        printf("kid");
+
+    }
+    return 0;
+}
